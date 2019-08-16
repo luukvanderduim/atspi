89081fd7d337b4861d5ce5357e811b797a4ef95f
Luuk van der Duim <luukvanderduim@gmail.com>
2019-08-16 14:43:18 +0200

    first commit

---
+++ atspi-sys/README.md
@@ -0,0 +1,12 @@
+# atspi-sys
+
+Gir generated raw bindings for atspi
+
+ Post Gir FIXes:
+
+ extern crate libdbus_sys as dbus;
+ Added libdbus dependency in Gir.toml
+
+ Added use glib::GTimeVal
+ Added type alias for GTimeVal
+ Added type alias  _AtspiAccessible = _AtspiAccessible;
---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+// Copyright 2013-2018, The Gtk-rs Project Developers.
+// See the COPYRIGHT file at the top-level directory of this distribution.
+// Licensed under the MIT license, see the LICENSE file or <http://opensource.org/licenses/MIT>
+
+//! # ATSPI bindings
+//!
+//! This library contains safe Rust bindings for ATSPI
+
+extern crate libc;
+#[macro_use]
+extern crate bitflags;
+
+extern crate atspi_sys;
+extern crate glib_sys;
+extern crate gobject_sys;
+#[macro_use]
+extern crate glib;
+
+#[macro_use]
+mod rt;
+
+use atspi_sys::*;
+mod auto;
+pub use auto::*;
+
+use glib::Error;
