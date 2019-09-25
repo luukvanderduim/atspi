444eb14072b60e937318deda262b8f80f9dfe5ac
Luuk van der Duim <luukvanderduim@gmail.com>
2019-09-25 13:29:47 +0200

    Dividing atspi-sys and atspi into separate ones.

--- atspi-sys/src/accessible.rs
+++
@@ -1,3 +0,0 @@
-// The _AtspiAccessible type definition is missing, yet it is used
-
-pub(crate) type _AtspiAccessible = crate::AtspiAccessible;
--- atspi-sys/src/timeval.rs
+++
@@ -1,6 +0,0 @@
-// ATSPI FFI needs a timeval implementation with Debug implemented
-//
-
-extern crate nix;
-
-pub type timeval = nix::sys::time::TimeVal;
