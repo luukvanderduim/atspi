c8d5375ef1e23413f12da497eca364db966b7737
Luuk van der Duim <luukvanderduim@gmail.com>
2019-10-02 16:26:39 +0200

    fix lints

--- src/other.rs
+++ src/other.rs
@@ -1,17 +1,5 @@
-/* fn clear_selection(&self) -> Result<(), Error> {
-    unsafe {
-        let mut error = ptr::null_mut();
-        let _ = atspi_sys::atspi_selection_clear_selection(self.as_ref().to_glib_none().0, &mut error);
-        if error.is_null() { Ok(()) } else { Err(from_glib_full(error)) }
-    }
-} */
-
 use crate::Accessible;
 use glib::translate::*;
-
-pub fn get_desktop_count() -> i32 {
-    unsafe { atspi_sys::atspi_get_desktop_count() as i32 }
-}
 
 pub fn get_desktop(i: i32) -> Option<Accessible> {
     unsafe {
@@ -22,4 +10,8 @@ pub fn get_desktop(i: i32) -> Option<Accessible> {
             Some(from_glib_full(ret))
         }
     }
+}
+
+pub fn get_desktop_count() -> i32 {
+    unsafe { atspi_sys::atspi_get_desktop_count() as i32 }
 }
